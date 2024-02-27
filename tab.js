eda9ff1443bc9fffc8afabbbf0b3b556e7510289
이종찬 <53935322+qkskfka@users.noreply.github.com>
2024-02-27 23:42:42 +0900

    tab, for

--- tab.js
+++ tab.js
@@ -1,3 +1,26 @@
-$('.tab-button').eq(0).on('click', function(){
-  
-});
+
+for (let i = 0; i< 3; i++){
+    
+    $('.tab-button').eq(i).on('click', function(){
+      $('.tab-button').removeClass('orange');
+      $('.tab-button').eq(i).addClass('orange');
+      $('.tab-content').removeClass('show');
+      $('.tab-content').eq(i).addClass('show');
+    });
+}
+
+
+
+//$('.tab-button').eq(1).on('click', function(){
+//  $('.tab-button').removeClass('orange');
+//  $('.tab-button').eq(1).addClass('orange');
+//  $('.tab-content').removeClass('show');
+//  $('.tab-content').eq(1).addClass('show');
+//});
+//
+//$('.tab-button').eq(2).on('click', function(){
+//  $('.tab-button').removeClass('orange');
+//  $('.tab-button').eq(2).addClass('orange');
+//  $('.tab-content').removeClass('show');
+//  $('.tab-content').eq(2).addClass('show');
+//});
